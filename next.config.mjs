86a3cfd98643f33abcbaeb604782a3ff7cd8b4d7
Ozsumit <129023326+Ozsumit@users.noreply.github.com>
2024-08-09 23:53:16 +0545

    alphaA

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  basePath: "/Nox",
+  output: "export", // <=== enables static exports
+  reactStrictMode: true,
+};
 export default nextConfig;
